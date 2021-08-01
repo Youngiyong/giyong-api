e8101c84450fbdf149fc5ac888267ec4aac1610e
giyong <youn9354@naver.com>
2021-08-01 22:28:32 +0900

    pagination add

---
+++ giyong/paginations.py
@@ -0,0 +1,36 @@
+from django.core.paginator import Paginator
+from rest_framework import pagination
+
+from lastorder.responses import LastorderResponse
+
+
+class RawPageNumberPagination(Paginator):
+    def __init__(self, object_list, per_page, count, **kwargs):
+        super().__init__(object_list, per_page, **kwargs)
+        self.raw_count = count
+
+    def _get_count(self):
+        return self.raw_count
+        count = property(_get_count)
+
+    def page(self, number):
+        number = self.validate_number(number)
+        return self._get_page(self.object_list, number, self)
+
+
+class PageNumberPagination(pagination.PageNumberPagination):
+    def get_paginated_response(self, data, page=None, has_next=None, count=None):
+        return Response(
+            data=data,
+            count=count if count else self.page.paginator.count,
+            has_next=has_next if has_next else self.page.has_next(),
+            page=page if page else self.page.number
+        )
+        # return LastorderResponse(data={
+        #         'links': {
+        #             'next': self.get_next_link(),
+        #             'previous': self.get_previous_link()
+        #         },
+        #         'count': self.page.paginator.count,
+        #         'results': data
+        # })
