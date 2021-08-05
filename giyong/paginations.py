b4f5ed311374c6b38a57c1e430f8aad57fd5dcd2
윤기용 <youn9354@naver.com>
2021-08-05 23:03:30 +0900

    pagination commit

--- giyong/paginations.py
+++ giyong/paginations.py
@@ -1,43 +1,40 @@
-from django.core.paginator import Paginator
 from rest_framework import pagination
-from giyong.responses import Response
+from responses import Response
 
+class Pagination(pagination.PageNumberPagination):
+    """
 
-class RawPageNumberPagination(Paginator):
-    def __init__(self, object_list, per_page, count, **kwargs):
-        super().__init__(object_list, per_page, **kwargs)
-        self.raw_count = count
+    """
+    page_size = 25
+    page_size_query_param = 'page'
+    max_page_size = 100
 
-    def _get_count(self):
-        return self.raw_count
-        count = property(_get_count)
+    def get_paginated_response(self, data):
 
-    def page(self, number):
-        number = self.validate_number(number)
-        return self._get_page(self.object_list, number, self)
+        page = {
+            "path": self.page_size_query_param,
+            "perPage": self.page.paginator.per_page,
+            "count": self.page.paginator.per_page,
+            "total": self.page.paginator.count,
+            "currentPage": int(self.request.GET.get("page", 1) or 1),
+            "lastPage": self.max_page_size,
+            "previousPageUrl": self.get_previous_link(),
+            'nextPageUrl': self.get_next_link()
+        }
+        # test = {
+        #     'path': self.get_previous_link(),
+        #     'perPage': self.page.number,
+        #     'count': self.page.count,
+        #     'total': self.page.count,
+        #     'currentPage': self.page_size,
+        #     'lastPage': "",
+        #     'previousPageUrl': "",
+        #     'nextPageUrl': self.get_next_link()
+        # }
+        # page = json.dumps(test)
 
-class BackOfficePaginator(Paginator):
-    def __init__(self, object_list, per_page, orphans=0,
-                 allow_empty_first_page=True):
-        self.object_list = object_list
-        self._check_object_list_is_ordered()
-        self.per_page = int(per_page)
-        self.orphans = int(orphans)
-        self.allow_empty_first_page = allow_empty_first_page
-
-class PageNumberPagination(pagination.PageNumberPagination):
-    def get_paginated_response(self, data, page=None, has_next=None, count=None):
         return Response(
             data=data,
-            count=count if count else self.page.paginator.count,
-            has_next=has_next if has_next else self.page.has_next(),
-            page=page if page else self.page.number
+            page=page
         )
-        # return LastorderResponse(data={
-        #         'links': {
-        #             'next': self.get_next_link(),
-        #             'previous': self.get_previous_link()
-        #         },
-        #         'count': self.page.paginator.count,
-        #         'results': data
-        # })
+
