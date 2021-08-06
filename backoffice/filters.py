321e4f3220a8f9306cf6741a411fd8de78c6ef34
윤기용 <youn9354@naver.com>
2021-08-06 23:45:36 +0900

    filters add

---
+++ backoffice/filters.py
@@ -0,0 +1,36 @@
+from django_filters import rest_framework as filters
+
+from models import(
+    Reviews,
+    ReviewImage,
+    ReviewWarnings,
+)
+
+class ReviewListFilter(filters.FilterSet):
+    """
+        백오피스 리뷰 관리 필터링
+    """
+    created_at = filters.DateFromToRangeFilter(method="get_date", help_text="날짜필터")
+
+    def get_date(self, queryset, name, value):
+        if value:
+            date = value
+            start = date.start_at
+            end = date.end_at
+            if start and not end:
+                queryset = queryset.filter(
+                    created__at__date__gte=start
+                ).values("created_at")
+
+            if not start and end:
+                queryset = queryset.filter(
+                    created__at__date__lte=end
+                )
+
+            if start and end:
+                queryset = queryset.filter(
+                    created_at__date__gte=start,
+                    created_at__date__lte=end
+                ).values("created_at")
+
+            return queryset
